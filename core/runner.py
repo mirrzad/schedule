343c04b7641882588ae23e809086fb108c078471
mirrzad <azadamir993@gmail.com>
2024-05-04 01:48:10 +0330

    add __str__, __repr__ methods for class Job

--- core/runner.py
+++ core/runner.py
@@ -1,11 +1,17 @@
+import time
+
 import schedule
 from schedule import repeat, every
 
 
-@repeat(every(10).seconds.tag('force'))
-def job():
+def job1(name, age=0):
     print("Hi Amir!")
 
 
-while True:
-    schedule.run_pending()
+schedule.every(2).seconds.do(job1, 'amir', age=30)
+
+f = schedule.get_jobs()
+
+print(repr(f[0]))
+print(str(f[0]))
+
--- core/schedule.py
+++ core/schedule.py
@@ -1,6 +1,9 @@
 import logging
 import datetime
 import time
+import random
+import pytz
+import re
 from collections.abc import Hashable
 from functools import partial, update_wrapper
 
@@ -38,7 +41,9 @@ class Scheduler:
             self._run_job(job)
 
     def _run_job(self, job):
-        job.run()
+        ret = job.run()
+        if isinstance(ret, CancelJob) or ret is CancelJob:
+            self.cancel_job(job)
 
     def get_jobs(self, tag=None):
         if tag is None:
@@ -89,12 +94,67 @@ class Job:
         self.period = None
         self.next_run = None
         self.last_run = None
+        self.latest = None
+        self.at_time = None
+        self.at_time_zone = None
+        self.start_day = None
         self.cancel_after = None
         self.tags = set()
         self.scheduler = scheduler
 
     def __lt__(self, other):
         return self.next_run < other.next_run
+
+    def __str__(self):
+        if hasattr(self.job_func, '__name__'):
+            job_func_name = self.job_func.__name__
+        else:
+            job_func_name = repr(self.job_func)
+        return 'job (interval={}, unit={}, do={}, args={}, kwargs={})'.format(
+            self.interval,
+            self.unit,
+            job_func_name,
+            '()' if self.job_func is None else self.job_func.args,
+            '{}' if self.job_func is None else self.job_func.keywords,
+        )
+
+    def __repr__(self):
+
+        time_stats = f'last_run: {self.last_run}, next_run: {self.next_run}'
+
+        if hasattr(self.job_func, '__name__'):
+            job_func_name = self.job_func.__name__
+        else:
+            job_func_name = repr(self.job_func)
+
+        if self.job_func is not None:
+            args = [x for x in self.job_func.args]
+            kwargs = ['%s=%s' % (k, v) for k, v in self.job_func.keywords.items()]
+            call_repr = job_func_name + '(' + ', '.join(args + kwargs) + ')'
+        else:
+            call_repr = [None]
+
+        if self.at_time is not None:
+            return 'Every %s %s at %s do %s %s' % (
+                self.interval,
+                self.unit[:-1] if self.interval == 1 else self.unit,
+                self.at_time,
+                call_repr,
+                time_stats
+            )
+
+        else:
+            fmt = (
+                'Every %(interval)s' + ('to %(latest)s' if self.latest is not None else '')
+                + ' %(unit)s do %(call_repr)s %(time_stats)s'
+            )
+            return fmt % dict(
+                interval=self.interval,
+                latest=self.latest,
+                unit=(self.unit[:-1] if self.interval == 1 else self.unit),
+                call_repr=call_repr,
+                time_stats=time_stats
+            )
 
     @property
     def second(self):
@@ -151,6 +211,135 @@ class Job:
         self.unit = 'weeks'
         return self
 
+    @property
+    def monday(self):
+        if self.interval != 1:
+            raise IntervalError(
+                'monday() is only for weekly jobs. Using monday() for every '
+                '2 or more weeks is not supported'
+            )
+        self.start_day = 'monday'
+        return self.weeks
+
+    @property
+    def tuesday(self):
+        if self.interval != 1:
+            raise IntervalError(
+                'tuesday() is only for weekly jobs. Using tuesday() for every '
+                '2 or more weeks is not supported'
+            )
+        self.start_day = 'tuesday'
+        return self.weeks
+
+    @property
+    def wednesday(self):
+        if self.interval != 1:
+            raise IntervalError(
+                'wednesday() is only for weekly jobs. Using wednesday() for every '
+                '2 or more weeks is not supported'
+            )
+        self.start_day = 'wednesday'
+        return self.weeks
+
+    @property
+    def thursday(self):
+        if self.interval != 1:
+            raise IntervalError(
+                'thursday() is only for weekly jobs. Using thursday() for every '
+                '2 or more weeks is not supported'
+            )
+        self.start_day = 'thursday'
+        return self.weeks
+
+    @property
+    def friday(self):
+        if self.interval != 1:
+            raise IntervalError(
+                'friday() is only for weekly jobs. Using friday() for every '
+                '2 or more weeks is not supported'
+            )
+        self.start_day = 'friday'
+        return self.weeks
+
+    @property
+    def saturday(self):
+        if self.interval != 1:
+            raise IntervalError(
+                'saturday() is only for weekly jobs. Using saturday() for every '
+                '2 or more weeks is not supported'
+            )
+        self.start_day = 'saturday'
+        return self.weeks
+
+    @property
+    def sunday(self):
+        if self.interval != 1:
+            raise IntervalError(
+                'sunday() is only for weekly jobs. Using sunday() for every '
+                '2 or more weeks is not supported'
+            )
+        self.start_day = 'sunday'
+        return self.weeks
+
+    def to(self, latest):
+        self.latest = latest
+        return self
+
+    def at(self, time_str, tz=None):
+        if self.unit not in ('days', 'hours', 'minutes') and not self.start_day:
+            raise ScheduleValueError('Invalid unit for the job')
+        if tz is not None:
+            if isinstance(tz, str):
+                self.at_time_zone = pytz.timezone(tz)
+            elif isinstance(tz, pytz.BaseTzInfo):
+                self.at_time_zone = tz
+            else:
+                raise ScheduleValueError('Timezone must be string or pytz.timezone object')
+        if not isinstance(time_str, str):
+            raise TypeError('at() parameter must be string')
+        if self.unit == 'days' or self.start_day:
+            if not re.match(r'^[0-2]\d:[0-5]\d(:[0-5]\d)?$', time_str):
+                raise ScheduleValueError('Invalid time format')
+        if self.unit == 'hours':
+            if not re.match(r'^([0-5]\d)?:[0-5]\d$', time_str):
+                raise ScheduleValueError('Invalid time format')
+        if self.unit == 'minutes':
+            if not re.match(r'^[0-5]\d$', time_str):
+                raise ScheduleValueError('Invalid time format')
+
+        time_values = time_str.split(':')
+
+        if len(time_values) == 3:
+            hour, minute, second = time_values
+        elif len(time_values) == 2 and self.unit == 'minutes':
+            hour, minute = 0, 0
+            _, second = time_values
+        elif len(time_values) == 2 and self.unit == 'hours' and len(time_values[0]):
+            hour = 0
+            minute, second = time_values
+        else:
+            hour, minute = time_values
+            second = 0
+
+        if self.unit == 'days' or self.start_day:
+            hour = int(hour)
+            if not (0 <= hour <= 23):
+                raise ScheduleValueError('Invalid number of hours')
+        elif self.unit == 'hours':
+            hour = 0
+            minute = int(minute)
+            if not (0 <= minute <= 59):
+                raise ScheduleValueError('Invalid number of minutes')
+        elif self.unit == 'minutes':
+            hour = 0
+            minute = 0
+            second = int(second)
+            if not (0 <= second <= 59):
+                raise ScheduleValueError('Invalid number of seconds')
+
+        self.at_time = datetime.time(hour=int(hour), minute=int(minute), second=int(second))
+        return self
+
     def tag(self, *tags):
         for tag in tags:
             if not isinstance(tag, Hashable):
@@ -198,30 +387,64 @@ class Job:
         return None
 
     def _is_overdue(self, when):
-        if self.cancel_after < when and self.cancel_after is not None:
+        if self.cancel_after is not None and self.cancel_after < when:
             return True
         else:
             return False
 
     def do(self, func, *args, **kwargs):
-        if self._is_overdue(datetime.datetime.now()):
-            logger.debug(f'Cancelling job {self}')
-            return CancelJob
-
         self.job_func = partial(func, *args, **kwargs)
         update_wrapper(self.job_func, func)
         self._schedule_next_run()
         self.scheduler.jobs.append(self)
-        if self._is_overdue(self.next_run):
-            logger.debug(f'Cancelling job {self}')
-            return CancelJob
         return self
 
     def _schedule_next_run(self):
         if self.unit not in ('seconds', 'minutes', 'hours', 'days', 'weeks'):
             raise ScheduleError('Invalid unit for the job')
-        self.period = datetime.timedelta(**{self.unit: self.interval})
+
+        if self.latest is not None:
+            if self.latest <= self.interval:
+                raise ScheduleValueError('Latest must be greater than interval')
+            else:
+                interval = random.randint(self.interval, self.latest)
+        else:
+            interval = self.interval
+        self.period = datetime.timedelta(**{self.unit: interval})
         self.next_run = datetime.datetime.now() + self.period
+
+        if self.start_day is not None:
+            if self.unit != 'weeks':
+                raise ScheduleValueError('Unit must be weeks')
+            weekdays = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
+            if self.start_day not in weekdays:
+                raise ScheduleValueError('Invalid day of the week. valid days are: {}'.format(weekdays))
+
+            weekday = weekdays.index(self.start_day)
+            days_ahead = weekday - self.next_run.weekday()
+
+            if days_ahead <= 0:
+                days_ahead += 7
+
+            self.next_run += datetime.timedelta(days_ahead) - self.period
+
+        if self.at_time is not None:
+            if self.unit not in ('days', 'hours', 'minutes') and self.start_day is None:
+                raise ScheduleValueError('Invalid unit for the time')
+
+            kwargs = {'second': self.at_time.second,
+                      'minute': self.at_time.minute,
+                      'hour': self.at_time.hour,
+                      'microsecond': 0
+                      }
+            self.next_run = self.next_run.replace(**kwargs)
+
+        if self.at_time_zone is not None:
+            self.next_run = self.at_time_zone.localize(self.next_run).astimezone().replace(tzinfo=None)
+
+        if self.start_day is not None and self.at_time is not None:
+            if (self.next_run - datetime.datetime.now()).days >= 7:
+                self.next_run -= self.period
 
     @property
     def should_run(self):
@@ -229,10 +452,16 @@ class Job:
         return self.next_run <= datetime.datetime.now()
 
     def run(self):
+        if self._is_overdue(datetime.datetime.now()):
+            logger.debug(f'Cancelling job {self}')
+            return CancelJob
         logger.debug(f'job {self} is running')
         output = self.job_func()
         self.last_run = datetime.datetime.now()
         self._schedule_next_run()
+        if self._is_overdue(self.next_run):
+            logger.debug(f'Cancelling job {self}')
+            return CancelJob
         return output
 
 
